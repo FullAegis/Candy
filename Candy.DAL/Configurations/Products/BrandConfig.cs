f1279e824b80238af17206f11bc8fd5884fb52e7
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-01 14:58:06 +0200

    ADD: BrandConfig

---
+++ Candy.DAL/Configurations/Products/BrandConfig.cs
@@ -0,0 +1,18 @@
+using Candy.DAL.Models.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Candy.DAL.Configurations.Products;
+
+public class BrandConfig : IEntityTypeConfiguration<Brand> {
+  public void Configure(EntityTypeBuilder<Brand> builder) {
+    builder.HasKey(x => x.Id)
+           .HasName("PK_Brand");
+    
+    builder.Property(x => x.Name)
+           .HasColumnName("Name")
+           .HasColumnType("nvarchar(128)")
+           .IsRequired()
+           ;
+  }
+};
