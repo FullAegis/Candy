c4a4e909ab48f2dfcfe56d26a0fb20b8aa2efeb7
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-01 14:21:57 +0200

    ADD: Category Config

---
+++ Candy.DAL/Configurations/Products/CategoryConfig.cs
@@ -0,0 +1,20 @@
+using Candy.DAL.Models.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Candy.DAL.Configurations.Products;
+
+public class CategoryConfig : IEntityTypeConfiguration<Category> {
+  public void Configure(EntityTypeBuilder<Category> builder) {
+    builder.HasKey(x => x.Id)
+           .HasName("PK_Category")
+           ;
+    
+    builder.Property(x => x.Name)
+           .HasColumnName("Name")
+           .HasColumnType("nvarchar(100)")
+           .IsRequired()
+           ;
+    
+  }
+};
