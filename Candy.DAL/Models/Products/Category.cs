7a026c96c35b6b6cbb88ab39bcd94ecd1cf0dfc3
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-01 14:17:59 +0200

    FIX: Category name length [1, 100]

--- Candy.DAL/Models/Products/Category.cs
+++ Candy.DAL/Models/Products/Category.cs
@@ -3,7 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace Candy.DAL.Models.Products {
   public record Category(in string name) {
     [Required] public required int Id { get; init; }
-    [Required] public required string Name { get; init; }
+    [Required] [StringLength(maximumLength: 100, MinimumLength = 1)]
+    public required string Name { get; init; }
   };
 }
   
