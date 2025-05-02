1d00102088fee8c8eb75e7b39c7746cab3d759d1
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-02 14:09:21 +0200

    FIX: Include

--- Candy.DAL/Models/User.cs
+++ Candy.DAL/Models/User.cs
@@ -1,26 +1,26 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Candy.DAL.Models.Orders;
 
 namespace Candy.DAL.Models {
-  
-public class User {
-  public required int Id { get; set; }
-  
-  [MaxLength(128)]
-  public string LastName { get; set; }
-  [MaxLength(128)]
-  public string FirstName { get; set; }
-  
-  [Required]
-  [EmailAddress]
-  public required string Email { get; set; }
-  
-  [Required]
-  [PasswordPropertyText]
-  public required string Password { get; set; }
-  
-  // [User] 1 —— Places ——> * [Order]
-  public ICollection<Order> Orders { get; set; }
-};
+  public class User {
+    public required int Id { get; set; }
+    
+    [MaxLength(128)]
+    public string LastName { get; set; }
+    [MaxLength(128)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+    
+    [Required]
+    [PasswordPropertyText]
+    public required string Password { get; set; }
+    
+    // [User] 1 —— Places ——> * [Order]
+    public ICollection<Order> Orders { get; set; }
+  };
 }
