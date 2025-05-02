e3b1a6b064938213af0bc1c3a56cb40f63967d32
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-02 10:28:31 +0200

    Formating

--- Candy.DAL/Configurations/UserConfig.cs
+++ Candy.DAL/Configurations/UserConfig.cs
@@ -6,23 +6,22 @@ namespace Candy.DAL.Configurations {
 
   public class UserConfig : IEntityTypeConfiguration<User> {
     public void Configure(EntityTypeBuilder<User> builder) {
-      builder.ToTable(nameof(User));
-
+      builder.ToTable(nameof(User))
+             ;
       builder.HasIndex(u => u.Email)
-        .IsUnique()
-        .HasDatabaseName("UK_Utilisateur_Email");
-
+             .IsUnique()
+             .HasDatabaseName("UK_Utilisateur_Email")
+             ;
       builder.Property(u => u.Password)
-        .HasColumnType("nvarchar(100)")
-        .IsRequired();
-
+             .HasColumnType("nvarchar(100)")
+             .IsRequired()
+             ;
       builder.Property(u => u.FirstName)
-        .HasColumnType("nvarchar(100)")
-        ;
-
+             .HasColumnType("nvarchar(100)")
+             ;
       builder.Property(u => u.LastName)
-        .HasColumnType("nvarchar(100)")
-        ;
+             .HasColumnType("nvarchar(100)")
+             ;
     }
   };
 }
