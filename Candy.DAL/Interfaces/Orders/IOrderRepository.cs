90853ad5b5c9c98a495294e5ea262e15f6b9d828
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-02 20:23:01 +0200

    ADD: (I)Order(Item)Repository

---
+++ Candy.DAL/Interfaces/Orders/IOrderRepository.cs
@@ -0,0 +1,7 @@
+namespace Candy.DAL.Interfaces.Orders;
+using Dal = Candy.DAL.Models.Orders;
+
+public interface IOrderRepository : IRepository<Dal::Order> {
+  List<Dal::Order> GetUserOrders(int userId);
+  
+};
