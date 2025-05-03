8d737fad88317c46e1a4b58e2952252a604937dc
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-03 07:54:45 +0200

    ADD: Missing CRUD methods in  IUserService

--- Candy.BLL/Interfaces/IUserService.cs
+++ Candy.BLL/Interfaces/IUserService.cs
@@ -5,6 +5,10 @@ using Bll = Candy.BLL.Models;
 
 public interface IUserService {
   Bll::User Login([EmailAddress] in string email, in string password);
-  void Register(Bll::User user);
+  void Register(in Bll::User user);
+  IEnumerable<Bll::Orders.Order> Orders(int userId);
+  void Update(int id, in Bll::User user);
+  void Delete(int id);
+  void Delete(in Bll::User user);
 };
 }
