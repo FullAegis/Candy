9bab5b1060eef23157701f3f2f4e85c5b670fccd
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-04 04:53:37 +0200

    ADD: builder.ToTable(nameof(Brand));

--- Candy.DAL/Configurations/Products/BrandConfig.cs
+++ Candy.DAL/Configurations/Products/BrandConfig.cs
@@ -6,6 +6,8 @@ namespace Candy.DAL.Configurations.Products;
 
 public class BrandConfig : IEntityTypeConfiguration<Brand> {
   public void Configure(EntityTypeBuilder<Brand> builder) {
+    builder.ToTable(nameof(Brand))
+           ;
     builder.HasKey(x => x.Id)
            .HasName("PK_Brand")
            ;
