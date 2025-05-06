671b54c211e7da555568c20df8a89dd80812f082
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-06 14:11:38 +0200

    FIX: Not sur why it was required in the first place

--- Candy.API/Models/DTO/Products/Category.cs
+++ Candy.API/Models/DTO/Products/Category.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace Candy.API.Models.DTO.Products;
 
 public record struct Category(in string name) {
-  [Required] public required int Id { get; init; }
+  [Required] public int Id { get; init; }
   [Required] public required string Name { get; init; } = name;
 }
